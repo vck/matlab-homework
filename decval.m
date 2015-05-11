4bcf3a60fa316b523ac9739ec8edce87f57105ec
Vicky Vernando Dasta <vickydasta@ymail.com>
2015-05-11 17:49:35 +0700

    Create decval.m

---
+++ decval.m
@@ -0,0 +1,14 @@
+% author vickydasta
+% decide value that would be disp based on given decision system
+% display --> b 
+
+a = 1;
+b = 2;
+if a > b
+	disp(a)
+elif a < b
+	disp(a+b)
+else
+	disp(b)
+endif
+
